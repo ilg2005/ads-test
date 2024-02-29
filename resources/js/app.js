e6a2068f531b599c961640abc6bd878b4ee47ae8
ilg2005 <36738148+ilg2005@users.noreply.github.com>
2024-02-29 17:30:49 +0300

    отображение MyForm в app.blade.php

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,8 @@ import './bootstrap';
 import {createApp} from 'vue/dist/vue.esm-bundler.js';
 import MyForm from './components/MyForm.vue';
 
-const app = createApp({
-    components: {
-        MyForm,
-    }
-});
-
+const app = createApp({});
+app.component('my-form', MyForm);
 app.mount('#app');
 
 import Alpine from 'alpinejs';
---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vue App</title>
+</head>
+<body>
+<div id="app" class="py-10 mx-auto w-3/4">
+    <my-form></my-form>
+</div>
+
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+</body>
+</html>
